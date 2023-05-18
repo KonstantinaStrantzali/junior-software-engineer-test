5417252df3b6c921e2233030091770507a7fa613
Konstantina <konstrantz@gmail.com>
2023-05-18 10:56:27 +0100

    Added the boroughSearcher view function that makes a request to the  endpoint, retrieves the borough data, and passes it to the 'revenueSearcher.html' template for rendering.

--- your-code/coffeeShopRevenueApp/coffeeShopRevenueApp/views.py
+++ your-code/coffeeShopRevenueApp/coffeeShopRevenueApp/views.py
@@ -1,1 +1,12 @@
+import requests
+from django.shortcuts import render, redirect, get_object_or_404
+from django import forms
+
+def boroughSearcher(request):
+    response = requests.get('http://localhost:3000/allboroughs')
+    data = response.json()
+    boroughs = data["borough"]
+    borough_names = [item['borough'] for item in boroughs]
+    context = {'boroughs': borough_names}
+    return render(request, 'revenueSearcher.html', context)
 
