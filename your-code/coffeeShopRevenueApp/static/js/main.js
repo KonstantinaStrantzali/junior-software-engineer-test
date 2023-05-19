ff6e90b16d259b831354c24c059f1a3d930aa6ce
Konstantina <konstrantz@gmail.com>
2023-05-19 11:06:29 +0100

    Completed last touches on HTML design, code refactoring, indentation,spelling errors, tweaks, bug fixes, readme.md update.
    - Finalized HTML design to enhance the visual appeal and user experience.
    - Conducted code refactoring for improved readability and maintainability.
    - Ensured consistent indentation across HTML & Javascript files for better code organization.
    - Rectified spelling errors and typos in comments, labels, and content.
    - Implemented minor tweaks to enhance responsiveness and usability.
    - Addressed reported bugs and resolved identified issues.

--- your-code/coffeeShopRevenueApp/static/js/main.js
+++ your-code/coffeeShopRevenueApp/static/js/main.js
@@ -2,7 +2,9 @@ const boroughSelect = document.getElementById("borough");
 const selectedBorough = document.getElementById("selected-borough");
 const tableContainer = document.getElementById("table-container");
 const total = document.getElementById("total-revenues");
+let totalRevenue = 0;
 
+// fetch data
 let coffeeshops = [];
 async function fetchAllCoffeeShops(){
     const response = await fetch("http://localhost:3000/coffeeshops");
@@ -15,14 +17,13 @@ async function fetchAllCoffeeShops(){
     borough.disabled = false;
 }
 fetchAllCoffeeShops();
-boroughSelect.addEventListener("change",  (event) => {
 
+boroughSelect.addEventListener("change",  (event) => {
     try {
         const boroughSelect = event.target.value;
         selectedBorough.textContent = boroughSelect;
         // Filter the coffee shops based on the selected borough
         const filteredCoffeeshops = coffeeshops.filter((coffeeshop) => coffeeshop.borough === boroughSelect);
-
         const table = document.createElement("table");
         table.classList.add("table");
         // Create the header row
@@ -32,24 +33,22 @@ boroughSelect.addEventListener("change",  (event) => {
         const headerShop = document.createElement("th");
         const headerBorough = document.createElement("th");
         const headerRevenue = document.createElement("th");
-
         // Set the header cell content
         headerShop.textContent = "Shop";
         headerBorough.textContent = "Borough";
         headerRevenue.textContent = "Revenue";
-
         // Append header cells to the header row
         headerRow.appendChild(headerShop);
         headerRow.appendChild(headerBorough);
         headerRow.appendChild(headerRevenue);
-
         // Append the header row to the table
         table.appendChild(headerRow);
-
+        // Reset the total revenue
+        totalRevenue = 0;
         filteredCoffeeshops.forEach((coffeeshop) => {
             // Remove any existing table
             const existingTable = tableContainer.querySelector("table");
-
+            totalRevenue += coffeeshop.revenue;
             if (tableContainer) {
                 // Remove any existing table
                 const existingTable = tableContainer.querySelector("table");
@@ -57,34 +56,28 @@ boroughSelect.addEventListener("change",  (event) => {
                     tableContainer.removeChild(existingTable);
                 }
             }
-
             // Create a row for the coffee shop data
             const dataRow = document.createElement("tr");
-
             // Create table cells for each data item
             const cellShop = document.createElement("td");
             const cellBorough = document.createElement("td");
             const cellRevenue = document.createElement("td");
-
             // Set the cell content with coffee shop data
             cellShop.textContent = coffeeshop.shop;
             cellBorough.textContent = coffeeshop.borough;
             cellRevenue.textContent = coffeeshop.revenue;
-
             // Append data cells to the data row
             dataRow.appendChild(cellShop);
             dataRow.appendChild(cellBorough);
             dataRow.appendChild(cellRevenue);
-
             // Append the data row to the table
             table.appendChild(dataRow);
-
             // Append the table to the document body or any desired container element
             tableContainer.appendChild(table);
+            // Update total
+            total.textContent = totalRevenue;
         });
     } catch (error) {
         console.error(error);
     }
-
-
 });
--- your-code/readme.md
+++ your-code/readme.md
@@ -1,6 +1,52 @@
-# Answer Code
+# Coffee Shop Revenue App
 
-Please create your local code in this folder.
+The Coffee Shop Revenue App is a web application built with Python, Django, JavaScript, HTML5, and CSS3. This application primarily functions as an
+interface for people to work with the API running in API servers.
 
-1. Add instructions on how to "build" or "run" your code so that we can view your work.
-2. Mention the features and why you came up with these ideas.
+
+![Final project image home page](coffeeShopRevenueApp/static/images/app-picture.png)
+
+## **Technologies Used**
+
+### Languages Used
+
+* [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+* [CSS3](https://developer.mozilla.org/en-US/docs/Archive/CSS3#:~:text=CSS3%20is%20the%20latest%20evolution,flexible%20box%20or%20grid%20layouts.)
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [Python](https://www.python.org/)
+
+### Dependencies
+- Django	4.2.1	
+- asgiref	3.6.0	
+- certifi	2023.5.7
+- charset-normalizer	3.1.0	
+- idna	3.4
+- pip	22.3.1	
+- requests	2.30.0	
+- setuptools	65.5.1	
+- sqlparse	0.4.4	
+- tzdata	2023.3	
+- urllib3	2.0.2
+- wheel	0.38.4	
+
+
+# Getting Started
+1) Fork or git clone this repository. 
+git clone https://github.com/KonstantinaStrantzali/junior-software-engineer-test TEST-FOLDER
+2) Inside your local repository, install the dependencies in the server directory. This is the API server that serves the data.
+- cd TEST-FOLDER
+- cd server
+- npm install
+Run the API server in dev mode
+ ``` npm run dev -- --open ```
+This should run the API server in http://localhost:3000/
+3) Create a virtual environment using virtualenv: ``` virtualenv venv ```
+
+4) Activate the virtual environment:
+- For Unix/Linux:``` source venv/bin/activate ```
+- For Windows: ``` venv\Scripts\activate ```
+
+
+5) Install the project dependencies: ``` pip install -r requirements.txt```
+
+6) Run the server ```python manage.py runserver```
